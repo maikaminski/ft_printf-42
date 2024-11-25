255308f0d7e60a61d03453eb6a4bea86caa2833a
maikaminski <makamins@c1r3p3.42sp.org.br>
2024-11-25 15:52:26 -0300

    finalizações

--- ft_hundle_pointer.c
+++ ft_hundle_pointer.c
@@ -6,7 +6,7 @@
 /*   By: makamins <makamins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 14:30:17 by makamins          #+#    #+#             */
-/*   Updated: 2024/11/25 14:37:16 by makamins         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:47:06 by makamins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_handle_pointer(unsigned long ptr)
 	int	count;
 
 	count = 0;
+	if (ptr == 0)
+		return (ft_putstr("(nil)"));
 	count += ft_putstr("0x");
 	return (count + ft_putnbr_hex(ptr, "0123456789abcdef"));
 }
--- ft_putstr.c
+++ ft_putstr.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_pustr.c                                         :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: makamins <makamins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 14:27:13 by makamins          #+#    #+#             */
-/*   Updated: 2024/11/25 14:37:27 by makamins         ###   ########.fr       */
+/*   Updated: 2024/11/25 15:43:10 by makamins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_putstr(char *s)
 	int	count;
 
 	if (!s)
-		return (ft_putstr("null"));
+		return (ft_putstr("(null)"));
 	count = 0;
 	while (*s)
 		count += ft_putchar(*s++);
